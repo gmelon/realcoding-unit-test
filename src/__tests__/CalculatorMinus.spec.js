e8400a1b26661a068d27f1eaf1823afe1f731174
wjdrud2532 <wjdrud2532@naver.com>
2022-05-02 16:54:41 +0900

    minus test3

--- src/__tests__/CalculatorMinus.spec.js
+++ src/__tests__/CalculatorMinus.spec.js
@@ -1,4 +1,4 @@
-import CalculatorMinus from "../js/calculator/Calculator";
+import Calculator from "../js/calculator/Calculator";
 
 describe("사직연산 뺄셈 테스트.", () => {
     test("3과 5를 빼면 -2이다.", () => {
