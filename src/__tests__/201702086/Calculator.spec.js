2b2730a463657d87702a3fe1a83773a294eb5bd8
wjdrud2532 <wjdrud2532@naver.com>
2022-05-10 17:06:50 +0900

    5.2 실전코딩 추가과제

--- src/__tests__/201702086/Calculator.spec.js
+++ src/__tests__/201702086/Calculator.spec.js
@@ -45,13 +45,26 @@ beforeEach(() => {
 
     describe("가상 함수 테스트해보기", () => { 
         test("customCalculation는 전달 된 함수를 호출시킨다.", () => {
+            const calculator = new Calculator();
+            const mockFunction = jest.fn();
+            calculator.customCalculation(mockFunction);
+            expect(mockFunction).toHaveBeenCalled();
+        
         
     });
-        test("customCalculation에 함수와 1, 2, 3인자를 전달하면, 전달 된 함수가 인자 1, 2, 3을 받아 호출한다.", () => {
-             
-        });
+    test("customCalculation는 전달 된 2, 3, 4를 호출시킨다.", () => {
+
+        const calculator = new Calculator();
+        const mockFunction = jest.fn();
+        calculator.customCalculation(mockFunction, 2, 3, 4); 
+        expect(mockFunction).toHaveBeenCalledWith(2, 3, 4);
+     
+ });
         test("abs함수는 Math.abs를 호출한다.", () => {
-        //
+            const calculator = new Calculator();
+            const mockMathAbs = jest.spyOn(Math, "abs"); 
+            calculator.abs(3); 
+            expect(mockMathAbs).toHaveBeenCalled();
      });
         });
 
